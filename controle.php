9b2f182da23772f1edcd48a0454f143b1274c42e
williansbeato <willians_beato@hotmail.com>
2020-02-17 11:06:31 -0300

    Cadastro php

---
+++ controle.php
@@ -0,0 +1,110 @@
+<?php
+
+	function imprime() {
+
+
+
+		$pessoas = array();
+		$fp = fopen('pessoas.txt', 'r');
+
+        if ($fp) {
+
+            while(!feof($fp)) {
+				$arr = array();
+                $cpf = fgets($fp);
+				$dados = fgets($fp);
+				if(!empty($dados)) {
+					$arr = explode("#", $dados);
+					$pessoas[$cpf] = $arr;
+				}
+			}
+
+			// fclose($fp);
+		}
+		
+		foreach ($pessoas as $cpf => $dados) {
+
+			if(!empty($dados)) {
+				echo "<tr>";
+					echo "<td>".$cpf."</td>";
+
+					foreach ($dados as $valor) {
+						echo "<td>".$valor."</td>";
+					}
+
+					echo "<td>";
+						echo "<button type='submit' name='acao' value='alterar/'".$cpf.">";
+							echo "<span class='glyphicon glyphicon-pencil'></span>";
+						echo "</button>";
+						echo "&nbsp";
+						echo "<button type='submit' name='acao' value='remover/'".$cpf.">";
+							echo "<span class='glyphicon glyphicon-remove'></span>";
+						echo "</button>";
+					echo "</td>";
+				echo "</tr>";
+			}
+		}
+
+	}
+
+
+	function cadastrar($post) {
+		
+		// Monta o array
+		$dados = array(
+			$post['cpf'] => array(
+				"nome" => $post['nome'],
+				"endereco" => $post['endereco'],
+				"telefone" => $post['telefone']
+				
+			)
+		);
+		print_r($dados);
+		cadastrou($dados);
+
+	}
+
+
+	function cadastrou($arr) {
+
+		$fp = fopen('pessoas.txt', 'a+');
+
+		if ($fp) {
+			foreach($arr as $cpf => $dados) {
+				if(!empty($dados)) {
+					$linha = $cpf."\n".$dados['nome']."#".$dados['endereco']."#".$dados['telefone'];
+					fputs($fp, "$linha\n");
+				}
+			}
+			fclose($fp);
+		}
+		print_r($linha);
+		echo "[OK] Dados escritos com Sucesso!";
+	}
+
+
+
+	function rotas($url) {
+
+		$dados = explode("/", $url);
+
+		// CADASTRAR
+		if(strcmp($dados[0], "cadastar") == 0) {
+			echo "<script> alert('CADASTRAR') </script>";
+		}
+		// ALTERAR
+		else if(strcmp($dados[0], "alterar") == 0) {
+			echo "<script> alert('ALTERAR') </script>";
+		}
+		// REMOVER
+		else if(strcmp($dados[0], "remover") == 0){
+			 echo "<script> alert('REMOVER') </script>";
+		}
+	}
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    include_once ("controle.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>SisCadPF</title>
+
+        <!-- Bootstrap URL - CSS -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link href="css/theme.css" rel="stylesheet">
+        <!-- Ajax Script -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.js" integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA=" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    </head>
+
+
+    <body role="document">
+    
+
+     <form class="form" method="post" action="viewRoute.php">
+        <input TYPE="hidden" NAME="form_submit" VALUE="OK">
+            <br><br>
+            <div class='row'>
+        		<div class='col-sm-4'>
+                    <button type="submit" name="acao" value="cadastar/0" class="btn btn-primary btn-block">
+                        <b>Cadastar</b>
+                    </button>
+                </div>
+                <div class='col-sm-4'>
+                    <button type="submit" name="acao" value="alterar/1" class="btn btn-success btn-block">
+                        <b>Alterar</b>
+                    </button>
+                </div>
+                <div class='col-sm-4'>
+                    <button type="submit" name="acao" value="remover/2" class="btn btn-danger btn-block">
+                        <b>Remover</b>
+                    </button>
+                </div>
+            </div>
+        </form>
+
+    <table class='table table-striped'>
+    		    <thead>
+    		        <tr>
+    		            <th>CPF</th>
+    		            <th>NOME</th>
+    		            <th>ENDEREÇO</th>
+    		            <th>TELEFONE</th>
+    		            <th>AÇÕES</th>
+    		        </tr>
+    		    </thead>
+    		    <tbody>
+                    <?php imprime(); ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
